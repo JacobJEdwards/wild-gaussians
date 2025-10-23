ef32d0ed2ebd1b7c863ef323d625f13c7646ac08
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-10-23 22:44:44 +0100

    run

---
+++ run.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import subprocess
+
+BASE_DIR = Path("/workspace/360_v2")
+OUTPUT_DIR = Path("/workspace/wild-gs")
+
+def main() -> None:
+    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+    for scene in BASE_DIR.iterdir():
+        if not scene.is_dir():
+            continue
+
+        scene_name = scene.name
+        output_scene_dir = OUTPUT_DIR / scene_name
+        output_scene_dir.mkdir(parents=True, exist_ok=True)
+
+        command = ["nerfbaselines", "train", "--method", "wild-gaussians", "--data", str(scene)]
+
+        print(f"Processing scene: {scene_name}")
+
+        subprocess.run(command, check=True, cwd=output_scene_dir)
+
+if __name__ == "__main__":
+    main()
