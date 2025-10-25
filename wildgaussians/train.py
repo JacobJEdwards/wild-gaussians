ed61a63eb23558ec48164d305cc25ea7a32582e8
JACOB EDWARDS <jacob.john1@talktalk.net>
2025-10-25 16:37:50 +0100

    changed config

--- wildgaussians/train.py
+++ wildgaussians/train.py
@@ -260,7 +260,7 @@ def train_command(
         load_dataset_fn = partial(
             load_dataset, 
             load_dataset_fn=load_colmap_dataset, 
-            images_path="images_8_contrast",
+            images_path="images_8_variance",
             evaluation_protocol=evaluation_protocol.get_name()
         )
 
